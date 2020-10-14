ed2ba90ab689ae902d11a7a50cd49360e991f1c4
Federico <federico2102@gmail.com>
2020-10-14 15:22:41 -0300

    -Logre solucionar lo del formato del html. Para eso cree archivos .css en la carpeta public y los asocie al html. -Sientanse libres de cambiar el estilo de la pagina como mas les guste. Yo solo puse el de ahora a modo de prueba.

--- resources/views/locacioneslist.blade.php
+++ resources/views/locacioneslist.blade.php
@@ -1,27 +1,31 @@
 <div class="card mb-3">
     <div class="card-body">
+        <h5 class="card-title">Lista de locaciones</h5>
 
         <table class="table">
-            <thead class="thead-dark">
+            <thead class="thead-light">
             <tr>
                 <th scope="col">Nombre</th>
                 <th scope="col">Capacidad</th>
                 <th scope="col">CapacidadMax</th>
                 <th scope="col">Geoposicion</th>
+
             </tr>
             </thead>
             <tbody>
             @foreach($locaciones as $locacion)
                 <tr>
-                    <td>{{$locacion->Nombre}}</td>
-                    <td>{{$locacion->Capacidad}}</td>
-                    <td>{{$locacion->CapacidadMax}}</td>
-                    <td>{{$locacion->Geoposicion}}</td>
+                    <td>{{ $locacion->Nombre }}</td>
+                    <td>{{ $locacion->Capacidad }}</td>
+                    <td>{{ $locacion->CapacidadMax }}</td>
+                    <td>{{ $locacion->Geoposicion }}</td>
                     <td>
-                        <a href="#" class="btn btn-sm btn-info">Show</a>
+
                         <a href="{{ url('/edit/'.$locacion->id) }}" class="btn btn-sm btn-warning">Edit</a>
-                        <a href="#" class="btn btn-sm btn-danger">Delete</a>
+
                     </td>
+
+
                 </tr>
             @endforeach
             </tbody>
