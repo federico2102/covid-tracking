2349eae90408761d01e11e1040d35de6cb5540a9
lucasfigarola <lucasadrielfigarola@gmail.com>
2020-10-20 01:48:19 -0300

    make it better

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,12 +1,20 @@
 <!doctype html>
+<x-app-layout>
 <html lang="en">
+<x-slot name="header">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Pricing example · Bootstrap</title>
+
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                  crossorigin="anonymous">
+            <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+    <title>Main</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/pricing/">
 
@@ -30,59 +38,85 @@
       }
     </style>
     <!-- Custom styles for this template -->
-    <link href="pricing.css" rel="stylesheet">
+    <link href="{{asset('css/pricing.css')}}" rel="stylesheet">
   </head>
+
   <body>
-  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">YoEstuveAhi</h1>
+      <p class="lead">Apps de trazabilidad ante contagios COVID</p>
+    </div>
+    
+    <div class="container">
+      <div class="card-deck mb-3 text-center">
+      <div class="row">
+          <div class="col-lg-3">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Locacion</h4>
+          </div>
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Registrar locacion</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary"><a href="location"> Locacion </a></button>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">CheckIn</h4>
+          </div>
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Realizar CheckIn</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">CheckIn</button>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">CheckOut</h4>
+          </div>
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Realizar CheckOut</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">CheckOut</button>
+          </div>
+        </div>
+      </div>
+        <div class="col-lg-3">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Contagiado</h4>
+          </div>
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Avisar estas contagiado</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Contagiado</button>
+          </div>
+        </div>
+        </div>
+      </div>
+      </div>
+    </div>
+    </div>
+    
+    <div class="card-footer">
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
       <h5 class="my-0 mr-md-auto font-weight-normal">YoEstuveAhi</h5>
       <nav class="my-2 my-md-0 mr-md-3">
         <h5 class="my-0 mr-md-auto font-weight-normal">Estado: </h5>
       </nav>
-      <a class="btn btn-outline-primary" href="#">No contagiado</a>
+      <a class="button b" href="#">No contagiado</a>
     </div>
-
-<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-  <h1 class="display-4">YoEstuveAhi</h1>
-  <p class="lead">Apps de trazabilidad ante contagios COVID</p>
-</div>
-
-<div class="container">
-  <div class="card-deck mb-3 text-center">
-    <div class="card mb-4 shadow-sm">
-      <div class="card-header">
-        <h4 class="my-0 font-weight-normal">Locacion</h4>
-      </div>
-      <div class="card-body">
-        <ul class="list-unstyled mt-3 mb-4">
-          <li>Registrar locacion</li>
-        </ul>
-        <button type="button" class="btn btn-lg btn-block btn-outline-primary"><a href="location"> Locacion </a></button>
-      </div>
     </div>
-    <div class="card mb-4 shadow-sm">
-      <div class="card-header">
-        <h4 class="my-0 font-weight-normal">CheckIn</h4>
-      </div>
-      <div class="card-body">
-        <ul class="list-unstyled mt-3 mb-4">
-          <li>Realizar CheckIn</li>
-        </ul>
-        <button type="button" class="btn btn-lg btn-block btn-primary">CheckIn</button>
-      </div>
-    </div>
-    <div class="card mb-4 shadow-sm">
-      <div class="card-header">
-        <h4 class="my-0 font-weight-normal">CheckOut</h4>
-      </div>
-      <div class="card-body">
-        <ul class="list-unstyled mt-3 mb-4">
-          <li>Realizar CheckOut</li>
-        </ul>
-        <button type="button" class="btn btn-lg btn-block btn-primary">CheckOut</button>
-      </div>
-    </div>
-  </div>
-
-</div>
 </body>
+</x-slot>
 </html>
+</x-app-layout>
