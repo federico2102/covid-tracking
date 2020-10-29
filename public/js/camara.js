de107dbcfe18ae997af481be72cc68afeb30bba5
Federico <federico2102@gmail.com>
2020-10-29 03:55:33 -0300

    Primeras pruebas de abrir la camara del celu haciendo click en check-in

--- public/js/camara.js
+++ public/js/camara.js
@@ -1,9 +1,3 @@
 function abrirCamara() {
-    if (
-        "mediaDevices" in navigator &&
-        "getUserMedia" in navigator.mediaDevices
-    ) {
-        // ok, browser supports it
-       navigator.mediaDevices.getUserMedia({video: true});
-    }
+       navigator.mediaDevices.getUserMedia({video: true, audio: false});
 }
