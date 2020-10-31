da5d1c529977e7d0a26e8a3acae495b1ebafcefa
Lucas Figarola <lucasadrielfigarola@gmail.com>
2020-10-31 16:35:34 -0300

    retocado main

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -49,8 +49,6 @@
 
     <div class="container">
       <div class="card-deck mb-3 text-center">
-      <div class="row">
-          <div class="col-lg-3">
         <div class="card mb-4 shadow-sm">
           <div class="card-header">
             <h4 class="my-0 font-weight-normal">Locacion</h4>
@@ -62,8 +60,6 @@
             <button type="button" class="btn btn-lg btn-block btn-outline-primary" onclick="location.href='location'">Locacion</button>
           </div>
           </div>
-        </div>
-        <div class="col-lg-3">
         <div class="card mb-4 shadow-sm">
           <div class="card-header">
             @if(Auth::user()->locacion == 0)
@@ -88,8 +84,6 @@
               <script src="{{ asset('js/camara.js') }}"></script>
           </div>
           </div>
-      </div>
-        <div class="col-lg-3">
         <div class="card mb-4 shadow-sm">
           <div class="card-header">
               @if(Auth::user()->estado == 'No contagiado')
@@ -137,8 +131,6 @@
         </div>
         </div>
         </div>
-      </div>
-      </div>
     </div>
     </div>
 
