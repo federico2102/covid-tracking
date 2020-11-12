1b8362cbfd73f1e7dfa43c3c5a76355f8ed2f469
Laura Muiño <lauramuino2@gmail.com>
2020-11-12 10:42:52 -0300

    vista un poco mejor de mail

--- resources/views/cuerpoMail.blade.php
+++ resources/views/cuerpoMail.blade.php
@@ -1,19 +1,115 @@
-<!DOCTYPE html>
-<html>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
 <head>
-    <title>Cuidado wacho</title>
-    <style>
-        /* Optional: Makes the sample page fill the window. */
-        html,
-        body {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            background-color: red;
-        }
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Demystifying Email Design</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <style type="text/css">
+        a[x-apple-data-detectors] {color: inherit !important;}
     </style>
+
 </head>
-<body>
-<h1 style="color:white; font-size:100px;">Compartiste locacion con un contagiado, asi que estas en riesgo wacho. Que cagada!</h1>
+<body style="margin: 0; padding: 0;">
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr>
+        <td style="padding: 20px 0 30px 0;">
+
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; border: 1px solid #cccccc;">
+                <tr>
+                    <td align="center" bgcolor="#70bbd9" style="padding: 0px 0 0px 0;">
+                        <img src="https://i.ibb.co/bLrmT0W/Capture.png" alt="Yo Estuve Ahí" width="100%" height="100%" style="display: block;" />
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+                            <tr>
+                                <td style="color: #153643; font-family: Arial, sans-serif;">
+                                    <h1 style="font-size: 24px; margin: 0;">Advertencia!</h1>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 20px 0 30px 0;">
+                                    <p style="margin: 0;">Un usuario de nuestra aplicación ha informado ser COVID positivo. Las ubicaciones que usted ha visitado, indican que estuvo en contacto con dicho usuario. Le recomendamos realizar un test y cumplir cuarentena estricta hasta obtener los resultados. Estas son las acciones posibles de nuestra aplicación de las que puede hacer uso ante esta situación.</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+                                        <tr>
+                                            <td width="260" valign="top">
+                                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+                                                    <tr>
+                                                        <td>
+                                                            <img src="https://i.ibb.co/1zQmqK6/Capture2.png" alt="" width="100%" height="140" style="display: block;" />
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 25px 0 0 0;">
+                                                            <p style="margin: 0;">En caso de ser COVID positivo, utilice esta opción para advertir a los usuarios que han tenido contacto con usted.</p>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                            <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+                                            <td width="260" valign="top">
+                                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+                                                    <tr>
+                                                        <td>
+                                                            <img src="https://i.ibb.co/vhNXcxg/Capture3.png" alt="" width="100%" height="140" style="display: block;" />
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 25px 0 0 0;">
+                                                            <p style="margin: 0;">Para revertir su estado de "contagiado", informe sobre su último test negativo. El sistema se encargará de habilitar las opciones usuales.</p>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#70bbd9" style="padding: 30px 30px;">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+                            <tr>
+                                <td style="color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;">
+                                    <p style="margin: 0;">&reg; Someone, somewhere 2025<br/>
+                                        <a href="#" style="color: #ffffff;">Unsubscribe</a> to this newsletter instantly</p>
+                                </td>
+                                <td align="right">
+                                    <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse;">
+                                        <tr>
+                                            <td>
+                                                <a href="http://www.twitter.com/">
+                                                    <img src="https://www.cineramageddon.com/wp-content/uploads/2020/06/twitter-logo.png" alt="Twitter." width="38" height="38" style="display: block;" border="0" />
+                                                </a>
+                                            </td>
+                                            <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+                                            <td>
+                                                <a href="http://www.twitter.com/">
+                                                    <img src="https://i.pinimg.com/originals/b1/85/9b/b1859bdac7d42d4a0a5a673bc4265564.png" alt="Facebook." width="38" height="38" style="display: block;" border="0" />
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+
+        </td>
+    </tr>
+</table>
 </body>
 </html>
+
